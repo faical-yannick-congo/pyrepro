585389a3812f0c5db22359c4684a315ee69e2c36
Yannick Congo <faical.congo@nist.gov>
2015-07-11 16:49:17 -0400

    First setup.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyRepro',
+    version='0.1dev',
+    packages=['pyrepro',],
+    license=open('LICENSE.md').read(),
+    long_description=open('README.md').read(),
+)
